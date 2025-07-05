f2806915dcc28d0fa65c6e2ad9962914dff0cc8f
Kortepk <sorokin-stepa@mail.ru>
2025-07-05 18:16:59 +0300

    Add view 3d model format (.obj), and rotate

--- ClawBot/MainWindow.xaml.cs
+++ ClawBot/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
 
 namespace ClawBot
 {
@@ -23,6 +14,56 @@ namespace ClawBot
         public MainWindow()
         {
             InitializeComponent();
+            LoadModel();
+            rotationSlider.ValueChanged += RotationSlider_ValueChanged;
         }
+        private void LoadModel()
+        {
+            try
+            {
+                // 1. Создаём импортёр моделей
+                var importer = new ModelImporter();
+
+                // 2. Указываем путь к модели (3 варианта)
+
+                // Вариант A: Из папки с исполняемым файлом
+                string modelPath = System.IO.Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "model/1.obj");
+
+                // Вариант B: Абсолютный путь (для теста)
+                // string modelPath = @"C:\Project\ClawBot\models\YourModel.obj";
+
+                // Вариант C: Из ресурсов (если добавили в проект)
+                // string modelPath = "pack://application:,,,/YourModel.obj";
+
+                // 3. Загружаем модель
+                var model = importer.Load(modelPath);
+
+                // 4. Добавляем в сцену
+                robotModel.Content = model;
+
+                // 5. Автоматически подгоняем камеру
+                viewport.ZoomExtents();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки модели:\n{ex.Message}",
+                               "ClawBot",
+                               MessageBoxButton.OK,
+                               MessageBoxImage.Error);
+            }
+        }
+
+        private void RotationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (robotModel.Content is Model3DGroup modelGroup)
+            {
+                var transform = new RotateTransform3D();
+                transform.Rotation = new AxisAngleRotation3D(new Vector3D(0, 1, 0), e.NewValue);
+                modelGroup.Transform = transform;
+            }
+        }
+
     }
 }
